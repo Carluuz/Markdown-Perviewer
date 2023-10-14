465850878efb4ccc865d2d2c464fb8f0c9364e64
Carluuz <carzz96@hotmail.com>
2023-10-15 00:44:19 +0100

    Changes to be committed: 	modified:   src/App.js 	new file:   src/defaultText

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 import Editor from './components/editor';
 import Preview from './components/preview'; 
 import './App.css';
+import defaultMarkdown from './defaultText'; 
+
+
+const u = "# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here's some other cool stuff:\n\nHeres some code, `<div></div>`, between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n```\n\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let's not forget embedded images:\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\n";
 
 
 const App = () => {
-  const [markdown, setMarkdown] = useState('hello world!');
+  const [markdown, setMarkdown] = useState(u);
 
   const handleChange = (e) => {
     setMarkdown(e.target.value);
@@ -18,5 +22,6 @@ const App = () => {
     </div>
   );
 }
+
 
 export default App;
